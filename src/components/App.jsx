a188c6508fba6222cb86fc3deb76a97947b54cc3
SerhiyDukh1984 <s.dux2011@ukr.net>
2022-07-26 23:18:40 +0300

    add 1 task

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import user from '../user';
+import data from '../data.json';
+
+export default function App() {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+    </div>
+  );
+}
